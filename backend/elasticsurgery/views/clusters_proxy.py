c6cb3f98750ebfa461ab3412f71b8c3a69716c6f
John Newman <john.newman@edited.com>
2019-09-25 16:51:56 +0100

    stub clusters endpoints

---
+++ backend/elasticsurgery/views/clusters_proxy.py
@@ -0,0 +1,11 @@
+from ..app import app
+
+
+@app.route('/api/clusters/<cluster_slug>/state/<metric_name>', methods=['GET'])
+def get_cluster_state(cluster_slug, metric_name):
+    pass
+
+
+@app.route('/api/clusters/<cluster_slug>/settings', methods=['GET'])
+def get_cluster_settings(cluster_slug):
+    pass
