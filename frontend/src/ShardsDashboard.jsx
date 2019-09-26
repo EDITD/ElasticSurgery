8976890fd577f1eb8ea8ac42bdcdc80b74e05a62
Chris Sinclair <christopher.sinclair@edited.com>
2019-09-26 11:08:46 +0100

    Added table view for shards

--- frontend/src/ShardsDashboard.jsx
+++ frontend/src/ShardsDashboard.jsx
@@ -4,50 +4,67 @@ import { connect } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { loadShardStatus } from './data/shards/actions';
+import { loadNodes } from './data/nodes/actions';
+import { isErrored, isLoading, isNotLoaded } from './data/utils';
+import Table from './Table';
 
-const mapStateToProps = ({ shards }) => ({
-	...shards,
+const mapStateToProps = ({ shards, nodes }) => ({
+    shards,
+    nodes,
 });
 
 const mapDispatchToProps = {
-	loadShardStatus,
+    loadShardStatus,
+    loadNodes,
 }
 
 class ShardsDashboard extends React.Component {
     static propTypes = {
-        loadingState: PropTypes.oneOf(['NOT_LOADED', 'LOADING', 'LOADED', 'ERROR']).isRequired,
-        data: PropTypes.object,
-        error: PropTypes.object,
+        shards: PropTypes.shape({
+            loadingState: PropTypes.oneOf(['NOT_LOADED', 'LOADING', 'LOADED', 'ERROR']).isRequired,
+            data: PropTypes.object,
+            error: PropTypes.object,
+        }).isRequired,
+        nodes: PropTypes.shape({
+            loadingState: PropTypes.oneOf(['NOT_LOADED', 'LOADING', 'LOADED', 'ERROR']).isRequired,
+            data: PropTypes.object,
+            error: PropTypes.object,
+        }).isRequired,
+        loadNodes: PropTypes.func.isRequired,
         loadShardStatus: PropTypes.func.isRequired,
     };
 
     componentDidMount() {
-        const { loadingState } = this.props;
-        if (loadingState === 'NOT_LOADED' || loadingState === 'ERROR') {
+        const { shards, nodes } = this.props;
+        if (isNotLoaded(shards) || isErrored(shards)) {
             this.props.loadShardStatus();
+        }
+        if (isNotLoaded(nodes) || isErrored(nodes)) {
+            this.props.loadNodes();
         }
     }
 
-    getContainerStyles() {
+    getContainerStyles(dataLoaded) {
         return {
             display: 'flex',
             margin: '0 auto',
-            width: '100%',
+            width: 'calc(100% - 240px)',
+            height: '100vh',
+            marginLeft: 240,
             alignItems: 'flex-start',
-            justifyContent: 'center',
+            justifyContent: dataLoaded ? 'flex-start' : 'center',
         };
     }
 
     render() {
-        const { loadingState } = this.props;
-
-        if (loadingState === 'NOT_LOADED' || loadingState === 'LOADING') {
+        const { shards, nodes } = this.props;
+        if (isNotLoaded(shards) || isLoading(shards) || isNotLoaded(nodes) || isLoading(nodes)) {
             return <div style={this.getContainerStyles()}>
                 <CircularProgress />
             </div>;
         }
 
-        if (loadingState === 'ERROR') {
+        if (isErrored(shards) || isErrored(nodes)) {
             return <div style={this.getContainerStyles()}>
                 <Typography variant="body1" component="p">
                     An error occurred loading the current status
@@ -55,8 +72,55 @@ class ShardsDashboard extends React.Component {
             </div>;
         }
 
-        return <div style={this.getContainerStyles()}>
-            <pre><code>{JSON.stringify(this.props.data, null, 2)}</code></pre>
+        const tableConfig = [
+            {
+                title: 'Index',
+                dataKey: 'index',
+                width: 300,
+            },
+            {
+                title: 'Shard #',
+                dataKey: 'shard',
+                width: 100,
+            },
+            {
+                title: 'Primary',
+                dataKey: 'primary',
+                width: 100,
+                formatter: isPrimary => isPrimary ? '\u2714' : '\u2718',
+            },
+            {
+                title: 'State',
+                dataKey: 'state',
+                width: 200,
+            },
+            {
+                title: 'Node',
+                dataKey: 'node',
+                flexGrow: 1,
+                formatter: nodeId => {
+                    const nodeData = nodes.data.nodes[nodeId];
+                    return nodeData ? nodeData.name : nodeId;
+                }
+            },
+        ];
+
+        const routingTable = this.props.shards.data.routing_table;
+        const indices = Object.keys(routingTable.indices);
+        const allShards = indices.reduce((allIndices, index) => {
+            const indexData = routingTable.indices[index];
+            const shardNames = Object.keys(indexData.shards);
+            const shards = shardNames.reduce((allShards, shard) => {
+                const shardData = indexData.shards[shard];
+                allShards.push(...shardData);
+                return allShards;
+            }, []);
+            allIndices.push(...shards);
+            return allIndices;
+        }, []);
+
+        return <div style={this.getContainerStyles(true)}>
+            <Table config={tableConfig} data={allShards} />
         </div>;
     }
 }
---
+++ frontend/src/data/nodes/actions.js
@@ -0,0 +1,31 @@
+export const NODE_ACTION_TYPES = {
+    LOAD_NODES: 'LOAD_NODES',
+    LOAD_NODES_SUCCESS: 'LOAD_NODES_SUCCESS',
+    LOAD_NODES_ERROR: 'LOAD_NODES_ERROR',
+};
+
+export function loadNodes() {
+    return async (dispatch) => {
+        dispatch({
+            type: NODE_ACTION_TYPES.LOAD_NODES,
+        });
+
+        try {
+            const response = await fetch('_cluster/state/nodes');
+            if (response.ok) {
+                const data = await response.json();
+                dispatch({
+                    type: NODE_ACTION_TYPES.LOAD_NODES_SUCCESS,
+                    data,
+                });
+            } else {
+                throw new Error(`Bad response from server: ${response.statusText}`);
+            }
+        } catch (error) {
+            dispatch({
+                type: NODE_ACTION_TYPES.LOAD_NODES_ERROR,
+                error,
+            });
+        }
+    }
+}
