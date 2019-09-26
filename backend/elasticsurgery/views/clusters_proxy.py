3a2ace5008eeb3fbf3ca2dd4ec90e6020d0c4b4f
Nick Barrett <nick@edited.com>
2019-09-26 11:10:39 +0100

    Add proper responses to cluster proxy endpoints.

--- backend/elasticsurgery/views/clusters_proxy.py
+++ backend/elasticsurgery/views/clusters_proxy.py
@@ -1,22 +1,27 @@
+from flask import jsonify
+
 from elasticsurgery.app import app
 from elasticsurgery.utils.elastic import pass_cluster_client
 from elasticsurgery.utils.request import get_request_data
 
 
-@app.route('/api/clusters/<cluster_slug>/state/<metric_name>', methods=['GET'])
+@app.route('/api/clusters/<cluster_slug>/state/<metric_name>', methods=('GET',))
 @pass_cluster_client
 def get_cluster_state(cluster_client, metric_name):
-    return cluster_client.cluster.state(metric_name)
+    state_data = cluster_client.cluster.state(metric_name)
+    return jsonify(**state_data)
 
 
-@app.route('/api/clusters/<cluster_slug>/settings', methods=['GET'])
+@app.route('/api/clusters/<cluster_slug>/settings', methods=('GET',))
 @pass_cluster_client
 def get_cluster_settings(cluster_client):
-    return cluster_client.cluster.get_settings()
+    settings_data = cluster_client.cluster.get_settings()
+    return jsonify(**settings_data)
 
 
-@app.route('/api/clusters/<cluster_slug>/settings', methods=['PUT'])
+@app.route('/api/clusters/<cluster_slug>/settings', methods=('PUT',))
 @pass_cluster_client
 def put_cluster_settings(cluster_client):
     settings = get_request_data()
-    return cluster_client.cluster.put_settings(body=settings)
+    new_settings = cluster_client.cluster.put_settings(body=settings)
+    return jsonify(updated=True, **new_settings)
