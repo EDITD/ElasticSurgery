95b647c5f13cbb0016bb80b09d4d14d315a21bf4
John Newman <john.newman@edited.com>
2019-09-26 12:10:52 +0100

    add create, delete and list endpoints

--- backend/elasticsurgery/views/clusters_config.py
+++ backend/elasticsurgery/views/clusters_config.py
@@ -1,16 +1,37 @@
-from ..app import app
+from flask import jsonify, request
+
+from elasticsurgery.app import app
+from elasticsurgery.settings import ES_INDEX_NAME
+from elasticsurgery.utils.elastic import get_state_es_client
 
 
 @app.route('/api/clusters', methods=['POST'])
 def create_cluster():
-    pass
+    es_client = get_state_es_client()
+    result = es_client.create(
+        index=ES_INDEX_NAME,
+        id=request.json['id'],
+        body=request.json['body'],
+    )
+    return jsonify(created=True, data=result), 201
 
 
 @app.route('/api/clusters/<cluster_slug>', methods=['DELETE'])
 def delete_cluster(cluster_slug):
-    pass
+    es_client = get_state_es_client()
+    es_client.delete(
+        index=ES_INDEX_NAME,
+        id=cluster_slug,
+    )
+    return jsonify(deleted=True), 201
 
 
 @app.route('/api/clusters', methods=['GET'])
 def list_clusters():
-    pass
+    es_client = get_state_es_client()
+    result = es_client.search(
+        index=ES_INDEX_NAME,
+        size=1000,
+    )
+    clusters = {hit['_id']: hit['_source'] for hit in result['hits']['hits']}
+    return jsonify(clusters=clusters), 200
