69868102a3e2add6b9c8f874739d0a635317bc87
Chris Sinclair <christopher.sinclair@edited.com>
2019-09-26 11:13:56 +0100

    Formatted ShardsDashboard

--- frontend/src/ShardsDashboard.jsx
+++ frontend/src/ShardsDashboard.jsx
@@ -14,50 +14,50 @@ const mapDispatchToProps = {
 }
 
 class ShardsDashboard extends React.Component {
-	static propTypes = {
-		loadingState: PropTypes.oneOf(['NOT_LOADED', 'LOADING', 'LOADED', 'ERROR']).isRequired,
-		data: PropTypes.object,
-		error: PropTypes.object,
-		loadShardStatus: PropTypes.func.isRequired,
-	};
+    static propTypes = {
+        loadingState: PropTypes.oneOf(['NOT_LOADED', 'LOADING', 'LOADED', 'ERROR']).isRequired,
+        data: PropTypes.object,
+        error: PropTypes.object,
+        loadShardStatus: PropTypes.func.isRequired,
+    };
 
-	componentDidMount() {
-		const { loadingState } = this.props;
-		if (loadingState === 'NOT_LOADED' || loadingState === 'ERROR') {
-			this.props.loadShardStatus();
-		}
-	}
+    componentDidMount() {
+        const { loadingState } = this.props;
+        if (loadingState === 'NOT_LOADED' || loadingState === 'ERROR') {
+            this.props.loadShardStatus();
+        }
+    }
 
-	getContainerStyles() {
-		return {
-			display: 'flex',
-			margin: '0 auto',
-			width: '100%',
-			alignItems: 'flex-start',
-			justifyContent: 'center',
-		};
-	}
+    getContainerStyles() {
+        return {
+            display: 'flex',
+            margin: '0 auto',
+            width: '100%',
+            alignItems: 'flex-start',
+            justifyContent: 'center',
+        };
+    }
 
     render() {
-		const { loadingState } = this.props;
+        const { loadingState } = this.props;
 
-		if (loadingState === 'NOT_LOADED' || loadingState === 'LOADING') {
-			return <div style={this.getContainerStyles()}>
-				<CircularProgress />
-			</div>;
-		}
+        if (loadingState === 'NOT_LOADED' || loadingState === 'LOADING') {
+            return <div style={this.getContainerStyles()}>
+                <CircularProgress />
+            </div>;
+        }
 
-		if (loadingState === 'ERROR') {
-			return <div style={this.getContainerStyles()}>
-				<Typography variant="body1" component="p">
-					An error occurred loading the current status
-				</Typography>
-			</div>;
-		}
+        if (loadingState === 'ERROR') {
+            return <div style={this.getContainerStyles()}>
+                <Typography variant="body1" component="p">
+                    An error occurred loading the current status
+                </Typography>
+            </div>;
+        }
 
         return <div style={this.getContainerStyles()}>
-			<pre><code>{JSON.stringify(this.props.data, null, 2)}</code></pre>
-		</div>;
+            <pre><code>{JSON.stringify(this.props.data, null, 2)}</code></pre>
+        </div>;
     }
 }
 
