645012b142331ed1d1e18d6ece55966a257b3d92
Nick Barrett <nick@edited.com>
2019-09-26 11:19:37 +0100

    Add new views to boot.

--- backend/boot.py
+++ backend/boot.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
-from elasticsurgery import views  # noqa F401
+
 from elasticsurgery.app import app
+from elasticsurgery.views import (  # noqa: F401
+    clusters_config,
+    clusters_proxy,
+    indices_proxy,
+)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
