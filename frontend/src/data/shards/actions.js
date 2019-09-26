eef73b42aa9915c2423ef9f76018f4f2c2e20698
Chris Sinclair <christopher.sinclair@edited.com>
2019-09-26 11:15:17 +0100

    Formatted shard actions

--- frontend/src/data/shards/actions.js
+++ frontend/src/data/shards/actions.js
@@ -7,26 +7,26 @@ export const SHARD_ACTION_TYPES = {
 export function loadShardStatus() {
     return async (dispatch) => {
         dispatch({
-			type: SHARD_ACTION_TYPES.LOAD_SHARD_STATUS,
-		});
+            type: SHARD_ACTION_TYPES.LOAD_SHARD_STATUS,
+        });
 
-		try {
-			// const response = await fetch('/api/state/<cluster_slug>/routing_table')
-			const response = await fetch('_cluster/state/routing_table');
-			if (response.ok) {
-				const data = await response.json();
-				dispatch({
-					type: SHARD_ACTION_TYPES.LOAD_SHARD_STATUS_SUCCESS,
-					data,
-				});
-			} else {
-				throw new Error(`Bad response from server: ${response.statusText}`);
-			}
-		} catch (error) {
-			dispatch({
-				type: SHARD_ACTION_TYPES.LOAD_SHARD_STATUS_ERROR,
-				error,
-			});
-		}
-	}
+        try {
+            // const response = await fetch('/api/state/<cluster_slug>/routing_table')
+            const response = await fetch('_cluster/state/routing_table');
+            if (response.ok) {
+                const data = await response.json();
+                dispatch({
+                    type: SHARD_ACTION_TYPES.LOAD_SHARD_STATUS_SUCCESS,
+                    data,
+                });
+            } else {
+                throw new Error(`Bad response from server: ${response.statusText}`);
+            }
+        } catch (error) {
+            dispatch({
+                type: SHARD_ACTION_TYPES.LOAD_SHARD_STATUS_ERROR,
+                error,
+            });
+        }
+    }
 }
