a1b20a7ff7a26d62ea11b945a7bd1306ed84b0b0
Gabriel Delavald <gabriel@edited.com>
2019-09-26 11:51:40 +0100

    Add react router to manage URL and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,35 @@ import React from 'react';
 import './App.css';
 import { AppBar, Toolbar, Typography, Drawer, List, ListItem, ListItemText } from '@material-ui/core';
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import ShardsDashboard from './ShardsDashboard.jsx'
 import NodesDashboard from './NodesDashboard.jsx'
 import Settings from './Settings.jsx'
+
+const routes = [
+    {
+        name: "Home",
+        path: "/",
+        exact: true,
+        component: () => "Welcome to ElasticSurgery",
+    },
+    {
+        name: "Shards",
+        path: "/shards",
+        component: ShardsDashboard,
+    },
+    {
+        name: "Nodes",
+        path: "/nodes",
+        component: NodesDashboard,
+    },
+    {
+        name: "Settings",
+        path: "/settings",
+        component: Settings,
+    }
+]
 
 class App extends React.Component {
     state = {
@@ -37,56 +63,54 @@ class App extends React.Component {
         </div>
     }
 
-    content = [
-        {
-            name: "Home",
-            component: this.welcomeText,
-        },
-        {
-            name: "Shards",
-            component: ShardsDashboard,
-        },
-        {
-            name: "Nodes",
-            component: NodesDashboard,
-        },
-        {
-            name: "Settings",
-            component: Settings,
-        }
-    ]
-
     render() {
-        const ComponentToRender = this.content.find(components => components.name === this.state.selectedContent).component;
-
         return (
-            <div className="App">
-                <AppBar position="static" style={this.styles.content}>
-                  <Toolbar>
-                    <Typography component="h1" variant="h6" color="inherit" noWrap>
-                      ElasticSurgery - Dashboard
-                    </Typography>
-                  </Toolbar>
-                </AppBar>
-                <Drawer
-                    variant="permanent"
-                    open={true}
-                    style={this.styles.drawer}
-                  >
-                  <div style={this.styles.drawer}>
-                    <List>
-                        {this.content.map(item => (
-                          <ListItem button key={item.name} onClick={() => this.menuClick(item.name)}>
-                              <ListItemText primary={item.name} />
-                         </ListItem>
-                        ))}
-                    </List>
-                </div>
+            <Router>
+                <div className="App">
+                    <AppBar position="static" style={this.styles.content}>
+                        <Toolbar>
+                            {routes.map((route, index) => (
+                                <Route
+                                    key={route.name}
+                                    index={index}
+                                    render={() => (
+                                        <Typography component="h1" variant="h6" color="inherit" noWrap>
+                                            ElasticSurgery - {route.name}
+                                        </Typography>
+                                    )}
+                                    path={route.path}
+                                    exact={route.exact}
+                                />
+                            ))}
+                        </Toolbar>
+                    </AppBar>
+                    <Drawer
+                        variant="permanent"
+                        open={true}
+                        style={this.styles.drawer}
+                      >
+                      <div style={this.styles.drawer}>
+                          <List>
+                              {routes.map(route => (
+                                  <Link to={route.path} key={route.name}>
+                                      <ListItem button>
+                                              <ListItemText primary={route.name} />
+                                      </ListItem>
+                                  </Link>
+                              ))}
+                          </List>
+                  </div>
               </Drawer>
               <main style={this.styles.content}>
-                  <ComponentToRender />
+                  {routes.map((route, index) => (
+                     <Route
+                       key={index}
+                       {...route}
+                     />
+                 ))}
               </main>
         </div>
+    </Router>
     );
     }
 }
