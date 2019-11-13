ca0068e0daf425fd8be4981a4d9e72c6cc293104
Nick Barrett <nick@edited.com>
2019-11-13 11:51:45 +0000

    Fix how the table sorts/filters/orders so we can sort before formatting.

--- frontend/src/Table.js
+++ frontend/src/Table.js
@@ -122,13 +122,16 @@ export default class Table extends Component {
         const { config, data } = this.props;
 
         this.setState({
-            data: applySort(
-                applySearch(
-                    applyFormatters([...data], config),
-                    searchTerms,
+            data: applySearch(  // search using formatted values
+                applyFormatters(  // format/pretty for user
+                    [...applySort(  // sort with raw input values
+                        data,
+                        orderBy,
+                        orderDirection,
+                    )],
+                    config,
                 ),
-                orderBy,
-                orderDirection,
+                searchTerms,
             ),
         });
     };
