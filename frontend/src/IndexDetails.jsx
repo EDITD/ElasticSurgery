e3bf3d7c9beed340060d73835f8e75fde006fe6e
Germain Chazot <germain@edited.com>
2019-12-12 15:45:43 +0000

    Remove unused import

--- frontend/src/IndexDetails.jsx
+++ frontend/src/IndexDetails.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
-import { Grid, Icon, Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import {
     Help,
     SentimentVerySatisfiedTwoTone,
