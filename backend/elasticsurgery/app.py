dee07e3f639a4f3aeefcf6b74695cb80830d62d5
Nick Barrett <nick@edited.com>
2020-06-23 16:02:51 +0100

    Use correct location for static/template directories.

--- backend/elasticsurgery/app.py
+++ backend/elasticsurgery/app.py
@@ -1,7 +1,18 @@
+from os import path
+
 from flask import Flask
 
 from . import settings
 
-app = Flask('elasticsurgery')
+
+frontend_build_dir = path.abspath(path.join(
+    path.dirname(__file__), '..', '..', 'frontend', 'build',
+))
+
+app = Flask(
+    'elasticsurgery',
+    static_folder=path.join(frontend_build_dir, 'static'),
+    template_folder=frontend_build_dir,
+)
 app.debug = settings.DEBUG
 app.secret_key = settings.SECRET_KEY
